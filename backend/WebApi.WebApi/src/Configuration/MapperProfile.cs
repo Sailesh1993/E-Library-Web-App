9e2b66d061ab2969cf3acebfbfce67e48acdaea9
Sailesh1993 <karki.saileshnep@gmail.com>
2023-11-24 19:11:05 +0200

    modify WebApi Layer, added src folder along with configuration,repoimplementation and database folder

---
+++ backend/WebApi.WebApi/src/Configuration/MapperProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using WebApi.Business.src.Dtos;
+using WebApi.Domain.src.Entities;
+
+namespace WebApi.WebApi.src.Configuration
+{
+    public class MapperProfile:Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<User, UserReadDto>();
+            CreateMap<UserUpdateDto, User>();
+            CreateMap<UserCreateDto, User>();
+        }
+    }
+}
