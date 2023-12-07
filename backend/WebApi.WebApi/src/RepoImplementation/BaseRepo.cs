70cf5706860d53013143a281167c1565d86a4b72
Sailesh1993 <karki.saileshnep@gmail.com>
2023-12-07 23:16:06 +0200

    creadted BaseRepo in framework layer

---
+++ backend/WebApi.WebApi/src/RepoImplementation/BaseRepo.cs
@@ -0,0 +1,35 @@
+using WebApi.Domain.src.Abstractions;
+using WebApi.Domain.src.Shared;
+
+namespace WebApi.WebApi.src.RepoImplementation
+{
+    public class BaseRepo<T> : IBaseRepo<T> where T : class
+    {
+
+        
+        public Task<T> CreateOne(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteOneById(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAll(QueryOptions queryOptions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetOneById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> UpdateOneById(T updatedEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
